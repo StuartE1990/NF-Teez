51c936b97895e8287b6142610f6655bef94632d5
Stuart <stuart.edwards1990@gmail.com>
2022-02-28 19:37:50 +0000

    setup

---
+++ nfteezreact/src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Components/Main.css'
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                The start
+            </div>
+        )
+    }
+}
+
+export default Main;
